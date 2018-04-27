6405e4763dc007cc2d80a83aa603f233c70d7b3c
Colton Carner <ccarner13@gmail.com>
2018-04-27 21:49:24 +1000

    edited

--- BingBot.py
+++ BingBot.py
@@ -2,24 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import random
 
-def searchNews():
+def searchNews(driver):
     element = driver.find_element_by_id("scp14").click()
 
-def normalSearch():
+def normalSearch(driver):
     driver.get("http://bing.com")
     assert "Bing" in driver.title
     elem = driver.find_element_by_name("q")
     elem.clear()
-    for i in range(random.randint(0,3)) # up to 3 chars in search box
-        elem.send_keys(chr(random.randint(ord('A'),ord('z'))
-    element.send_keys(Keys.ARROW_DOWN)
+
+    # between 1 and 3 chars in search box
+    for i in range(1,random.randint(0,3)): 
+        elem.send_keys( chr(random.randint(ord('A'),ord('z'))))
+    
+    elem.send_keys(Keys.ARROW_DOWN)
     elem.send_keys(Keys.RETURN)
+    
     #assert "No results found." not in driver.page_source
 
 def Main():
     driver=webdriver.Chrome("./drivers/chromedriver.exe")
-    normalSearch()
-    driver.close()    
+    normalSearch(driver)
+    #driver.close()    
 
 Main()
 
