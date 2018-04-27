df70ac8d82e265a483ee1de518011809f5c1f25d
Colton Carner <ccarner13@gmail.com>
2018-04-27 23:23:10 +1000

    moves window off screen when starting program

--- BingBot.py
+++ BingBot.py
@@ -11,6 +11,7 @@ def send_keys_delay_random(elem,keys,min_delay=0.05,max_delay=0.25):
         time.sleep(random.uniform(min_delay,max_delay))
 
 def searchNews(driver):
+    #incomplete
     element = driver.find_element_by_id("scp14").click()
 
 def normalSearch(driver):
@@ -19,11 +20,17 @@ def normalSearch(driver):
     elem = driver.find_element_by_name("q")
     elem.clear()
 
-    # between 1 and 3 chars in search box
-    for i in range(1,random.randint(0,3)): 
-        elem.send_keys( chr(random.randint(ord('A'),ord('z'))))
+    # between 1 and 2 chars in search box
+    for i in range(random.randint(1,2)): 
+        elem.send_keys( chr(random.randint(ord('a'),ord('z'))))
+        time.sleep(random.uniform(0.1,0.3))
+
+    #between 1 and 5 down arrow presses chars in search box
+    for i in range(random.randint(1,5)): 
+        elem.send_keys(Keys.ARROW_DOWN)
+        time.sleep(random.uniform(0.1,0.3))
     
-    elem.send_keys(Keys.ARROW_DOWN)
+    time.sleep(1)
     elem.send_keys(Keys.RETURN)
     
     #assert "No results found." not in driver.page_source
@@ -40,17 +47,24 @@ def login(driver):
     time.sleep(5)
     elem = driver.find_element_by_xpath('//*[@id="i0118"]') #select the password textbox element
     send_keys_delay_random(elem, "MicrosoftPassword")
+    time.sleep(1)
+    elem.send_keys(Keys.RETURN) # move to next screen
     time.sleep(3)
 
 def Main():
     chrome_options = Options()  
-    #chrome_options.add_argument("--headless")  
+    # for running chrome without a window?
+    #chrome_options.add_argument("--headless --disable-gpu")  
     driver=webdriver.Chrome(executable_path ="./drivers/chromedriver.exe", chrome_options = chrome_options)
+    driver.set_window_position(2000,0)
     login(driver)
-    normalSearch(driver)
+    for i in range (0,10):
+        normalSearch(driver)
+        time.sleep(random.uniform(5,20))
     time.sleep(10)
     #driver.close()    
 
+#run the damn program already!
 Main()
 
 
